de6f3a18bf2692271036655520d35775d47194e9
Sajad <skm.kiani@gmail.com>
2022-04-14 02:02:08 +0430

    update home

--- src/pages/home.js
+++ src/pages/home.js
@@ -6,22 +6,20 @@ import {Github} from "react-bootstrap-icons";
 
 export default function Home () {
     return (
-        <div>
-        <MyNav />
-        <Container>
-            <Row className={'vh-100 align-items-center justify-content-between justify-content-ms-center'}>
-                <Col className={'col-md-8 col-12'}>
+        <div className={'vh-100 d-flex flex-column'}>
+            <MyNav />
+            <div className={'d-flex container gap-5 flex-wrap flex-grow-1 justify-content-center align-items-center'}>
+                <div className={'col-10 col-md-8'}>
                     <h1 className={'fw-bolder display-1'}>Bookmark</h1>
                     <p className={'fw-light fs-3'}>Saving bookmarks allows you to easily access your favorite locations on the Web. It is helpful to bookmark frequently visited websites and useful references since you don't have to remember the URLs. Additionally, you can just click the bookmarks instead of typing in the full web addresses.</p>
-                </Col>
-                <Col className={'col-md-3 col-9 '}>
+                </div>
+                <div className={'col-md-3 col-10'}>
                     <Image fluid src={banner} alt={'banner'} />
-                </Col>
-            </Row>
-            <Row>
+                </div>
+            </div>
+            <div>
                 <p className={'fw-light text-center'}>©️CopyRight 2022 - <a id={'git'} href={'https://github.com/SajjadKiani/Bookmarker'}>github <Github/></a></p>
-            </Row>
-        </Container>
+            </div>
         </div>
     )
 }
